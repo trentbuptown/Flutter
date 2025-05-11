872c86911e16de38386293b383d5d200133f2f06
DESKTOP-2TH0TA6\Bao Tran <trangiabao256@gmail.com>
2025-05-12 00:02:18 +0700

    final

--- app_interview/lib/main.dart
+++ app_interview/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,6 +31,70 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
+
+    Future<List<Widget>> createList() async {
+      List<Widget> items = [];
+      String dataString =
+        await DefaultAssetBundle.of(context).loadString("assets/data.json");
+      List<dynamic> dataJSON = jsonDecode(dataString);
+
+
+        dataJSON.forEach((object){
+
+          String finalString = "";
+          List<dynamic> dataList = object["placeItems"];
+          dataList.forEach((item){
+            finalString = finalString + item + " | " ;
+          });
+
+          items.add(Padding(padding: EdgeInsets.all(2.0),
+          child: Container(
+
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 2.0,
+                  blurRadius: 5.0,
+                ),
+              ],
+            ),
+            margin: EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), bottomLeft: Radius.circular(10.0)),
+                  child: Image.asset(object["placeImage"],width: 80,height: 80, fit: BoxFit.cover,),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(object["placeName"]),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 2.0, bottom: 2.0),
+                        child: Text(finalString,overflow: TextOverflow.ellipsis ,style: TextStyle(fontSize: 12.0, color: Colors.black54),maxLines: 1,),
+                      ),
+                      Text("Min. Order: ${object["minOrder"]}", style: TextStyle(fontSize: 12.0, color: Colors.black54),)
+                    ],
+                  ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          ));
+        });
+        return items;
+    }
+
     // TODO: implement build
     return Scaffold(
         body: Container(
@@ -49,15 +115,35 @@ class MyHomePage extends StatelessWidget {
                       ],
                     ),
                     ),
-                    BannerWidgetArea()
+                    BannerWidgetArea(),
+                    Container(
+                      child: FutureBuilder(
+                          initialData: <Widget>[Text("")],
+                          future: createList(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData){
+                              return Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: ListView(
+                                  primary: false,
+                                  shrinkWrap: true,
+                                  children: snapshot.data ?? [],
+                                ),
+                              );
+                            }else{
+                              return CircularProgressIndicator();
+                            }
+                          }),
+                      ),
                   ],
-
                 ),
               )),
         ),
+      floatingActionButton: FloatingActionButton(onPressed: (){},
+          backgroundColor: Colors.black,
+      child: Icon(MdiIcons.food,color: Colors.white,)),
     );
   }
-
 }
 
 class BannerWidgetArea extends StatelessWidget {
@@ -97,8 +183,29 @@ class BannerWidgetArea extends StatelessWidget {
               child: Image.asset(
                 bannerImages[x],
                 fit: BoxFit.cover,
-            )
-            )
+            ),
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.transparent, Colors.black]),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(bannerItems[x], style: TextStyle(fontSize: 25.0, color: Colors.white),),
+                  Text("More than 40% Off", style: TextStyle(fontSize: 12.0, color: Colors.white),)
+                ],
+              ),
+            ),
           ],
       ),
       ),
